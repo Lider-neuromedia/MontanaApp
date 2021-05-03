973ee1604c6e2eab4bdfebb761b690752f7fbde3
Jose Nieto <inge1neuro@gmail.com>
2021-05-03 17:14:31 -0500

    Refactorizando dashboard de vendedor.

--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -9,10 +9,10 @@ import 'card_data.dart';
 class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    TextStyle titleStyle = Theme.of(context)
-        .textTheme
-        .headline5
-        .copyWith(color: CustomTheme.textColor1);
+    TextStyle titleStyle = Theme.of(context).textTheme.headline5.copyWith(
+          color: CustomTheme.textColor1,
+          fontWeight: FontWeight.w600,
+        );
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -22,7 +22,7 @@ class DashboardPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 10.0),
+            SizedBox(height: 30.0),
             Text('CONSOLIDADO COMISIONES', style: titleStyle),
             SizedBox(height: 20.0),
             CardData(
@@ -32,70 +32,30 @@ class DashboardPage extends StatelessWidget {
               color: CustomTheme.yellowColor,
               isMain: true,
             ),
-            commissionsList(),
-            SizedBox(height: 40.0),
+            _CommisionsList(),
+            SizedBox(height: 20.0),
             Text('CONSOLIDADO CLIENTES', style: titleStyle),
             SizedBox(height: 20.0),
-            consolidatedClients(),
+            _ConsolidatedClients(),
             SizedBox(height: 40.0),
             Text('CONSOLIDADO PEDIDOS', style: titleStyle),
             SizedBox(height: 20.0),
-            consolidatedOrders(),
+            _ConsolidatedOrders(),
             SizedBox(height: 50.0),
           ],
         ),
       ),
     );
   }
+}
 
-  Widget commissionsList() {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 20.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          CardData(
-            title: 'Próximas a perder',
-            value: '\$4.300.400',
-            icon: Icons.face,
-            color: CustomTheme.yellowColor,
-            isMain: false,
-          ),
-          CardData(
-            title: 'Comisiones ganadas',
-            value: '\$3.500.400',
-            icon: Icons.face,
-            color: CustomTheme.greenColor,
-            isMain: false,
-          ),
-        ],
-      ),
-    );
-  }
+class _ConsolidatedOrders extends StatelessWidget {
+  const _ConsolidatedOrders({
+    Key key,
+  }) : super(key: key);
 
-  Widget consolidatedClients() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        CardStatistic(
-          isMain: true,
-          title: 'Clientes Generados',
-          subtitle: 'Último mes',
-          value: 84,
-          label: 'Clientes',
-        ),
-        CardStatistic(
-          isMain: false,
-          title: 'Clientes Atendidos',
-          subtitle: 'Último mes',
-          value: 16,
-          label: 'Clientes',
-        ),
-      ],
-    );
-  }
-
-  Widget consolidatedOrders() {
+  @override
+  Widget build(BuildContext context) {
     PageController pageController = PageController(
       initialPage: 1,
       viewportFraction: 0.45,
@@ -123,6 +83,67 @@ class DashboardPage extends StatelessWidget {
             title: 'Rechazados',
             value: 20,
             color: CustomTheme.redColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ConsolidatedClients extends StatelessWidget {
+  const _ConsolidatedClients({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        CardStatistic(
+          isMain: false,
+          title: 'Clientes Generados',
+          subtitle: 'Último mes',
+          value: 84,
+          label: 'Clientes',
+        ),
+        CardStatistic(
+          isMain: false,
+          title: 'Clientes Atendidos',
+          subtitle: 'Último mes',
+          value: 16,
+          label: 'Clientes',
+        ),
+      ],
+    );
+  }
+}
+
+class _CommisionsList extends StatelessWidget {
+  const _CommisionsList({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CardData(
+            title: 'Próximas a perder',
+            value: '\$4.300.400',
+            icon: Icons.face,
+            color: CustomTheme.yellowColor,
+            isMain: false,
+          ),
+          CardData(
+            title: 'Comisiones ganadas',
+            value: '\$3.500.400',
+            icon: Icons.face,
+            color: CustomTheme.greenColor,
+            isMain: false,
           ),
         ],
       ),

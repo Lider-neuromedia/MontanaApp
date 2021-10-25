d440f2e53e5fbbbe4c354feb16daf18800a8831d
Jose Nieto <inge1neuro@gmail.com>
2021-10-25 15:35:54 -0500

    Validando error de conexión cuando obtiene sesión de usuario.

--- lib/providers/session_provider.dart
+++ lib/providers/session_provider.dart
@@ -1,6 +1,6 @@
-import 'package:montana_mobile/utils/preferences.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
+import 'package:montana_mobile/utils/preferences.dart';
 
 class SessionProvider {
   final String _url = dotenv.env['API_URL'];
@@ -8,13 +8,22 @@ class SessionProvider {
 
   Future<bool> isUserSessionValid() async {
     final url = Uri.parse('$_url/auth/getUserSesion');
-    final response = await http.get(url, headers: _preferences.signedHeaders);
 
-    if (response.statusCode == 403 || response.statusCode == 401) {
+    if (_preferences.token == null || _preferences.session == null) {
       _preferences.token = null;
       _preferences.session = null;
       return false;
     }
+
+    try {
+      final response = await http.get(url, headers: _preferences.signedHeaders);
+
+      if (response.statusCode == 403 || response.statusCode == 401) {
+        _preferences.token = null;
+        _preferences.session = null;
+        return false;
+      }
+    } catch (error) {}
 
     return true;
   }
